4defdbc4769067f5bb262842567a0ca5c02728f5
cryptokeepto <cryptokeepto@gmail.com>
2020-03-19 23:55:50 +0700

    Init project

---
+++ app.js
@@ -0,0 +1,27 @@
+(function() {
+  hello.init(
+    {
+      joinme: "acfwmje3yrhz3nmmpd943whd"
+    },
+    {
+      scope: "start_meeting",
+      redirect_uri: "http://178.128.100.249/",
+      state: "LIVE",
+      response_type: "code",
+      display: "popup"
+    }
+  );
+})();
+
+function login() {
+  hello.login("joinme", res => {
+    if (res.error) {
+      const { message } = res.error;
+      return alert(message);
+    } else {
+      const params = new URLSearchParams(window.location.search);
+      const code = params.get("code");
+      console.log(code);
+    }
+  });
+}
